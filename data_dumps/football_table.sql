2d71b2d2e765cfa0e618ec1477a4fdfb95e581e6
pasim <janis.janovskis@gmail.com>
2019-09-04 04:15:10 +0200

    New data set
    Really interesting

---
+++ data_dumps/football_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `futbol_stats` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `home_team` varchar(40) DEFAULT NULL,
+  `away_team` varchar(40) DEFAULT NULL,
+  `home_score` int(3) DEFAULT NULL,
+  `away_score` int(3) DEFAULT NULL,
+  `tournament` varchar(150) DEFAULT NULL,
+  `city` varchar(40) DEFAULT NULL,
+  `neutral` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41159 DEFAULT CHARSET=latin1;
